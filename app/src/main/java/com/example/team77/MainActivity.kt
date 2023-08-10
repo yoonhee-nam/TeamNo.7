db869f4b43e1d980a72377c5595aaa0a41c5996e
suyeno <realsu_yeonini418@naver.com>
2023-08-10 17:03:14 +0900

    수연

--- app/src/main/java/com/example/team77/MainActivity.kt
+++ app/src/main/java/com/example/team77/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        print("안녕하세요 저는 수연이에여")
     }
 }
