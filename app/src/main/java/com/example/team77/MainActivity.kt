2ffc0ee323dac3c6207bfe1b50ca60e92cdaf08b
yoonhee~ <flvps123@gmail.com>
2023-08-10 17:08:00 +0900

    ㄷ

--- app/src/main/java/com/example/team77/MainActivity.kt
+++ app/src/main/java/com/example/team77/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        println("윤희입니다.")
         setContentView(R.layout.activity_main)
     }
 }
