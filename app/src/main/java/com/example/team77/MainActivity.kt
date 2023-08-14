5b4d07aecdc34e3f9a5a569fb8468c8281732525
suyeno <realsu_yeonini418@naver.com>
2023-08-14 18:22:33 +0900

    my_page_second 초본 완성 (랜덤 이미지, 스크롤)

--- app/src/main/java/com/example/team77/MainActivity.kt
+++ app/src/main/java/com/example/team77/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.example.team77
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        print("안녕하세요 저는 수연이에여")
-
-        println("I'm juri")
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener{
+            val intent = Intent(this,MyPageActivity ::class.java)
+            startActivity(intent)
+        }
 
     }
 }
