743db0bc2bf19cec33ce8571c2cc284d0a15814e
Lim-Juri <imjuly05@gmail.com>
2023-08-14 20:37:14 +0900

    cw pink ver.

---
+++ app/src/main/java/com/example/team77/DetailActivity.kt
@@ -0,0 +1,23 @@
+package com.example.team77
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.content.res.ResourcesCompat
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.detail_activity)
+
+        val image = findViewById<ImageView>(R.id.imageView)
+        val im = when((1..4).random()){
+            1 -> R.drawable.cw_main
+            2 -> R.drawable.cy_main2
+            3 -> R.drawable.cy_main3
+            4 -> R.drawable.cy_main4
+            else -> R.drawable.cw_main
+        }
+        image.setImageDrawable(ResourcesCompat.getDrawable(resources, im, null))
+    }
+}
