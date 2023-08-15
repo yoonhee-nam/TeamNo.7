a7af41973fecfd5c43c0d5d0f846caed6a771b8d
yoonhee~ <flvps123@gmail.com>
2023-08-15 15:00:34 +0900

    email,password에 아이콘추가

--- NewCyworld/app/src/main/java/com/example/cyworld7/LogInActivity.kt
+++ NewCyworld/app/src/main/java/com/example/cyworld7/LogInActivity.kt
@@ -3,6 +3,8 @@ package com.example.cyworld7
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
