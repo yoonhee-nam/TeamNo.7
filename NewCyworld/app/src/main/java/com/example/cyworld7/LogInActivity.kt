09dab7a4707600ab549ccfc0807086ccc7460218
yoonhee~ <flvps123@gmail.com>
2023-08-15 15:46:12 +0900

    구글,네이버 연동 추가

--- NewCyworld/app/src/main/java/com/example/cyworld7/LogInActivity.kt
+++ NewCyworld/app/src/main/java/com/example/cyworld7/LogInActivity.kt
@@ -1,13 +1,16 @@
 package com.example.cyworld7
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,6 +50,20 @@ class LogInActivity : AppCompatActivity() {
         btn2.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
+        }
+
+        val btngoogle = findViewById<ConstraintLayout>(R.id.btngoogle)
+        btngoogle.setOnClickListener {
+            val address = "https://accounts.google.com/v3/signin/identifier?authuser=0&continue=https%3A%2F%2Fwww.google.com%2F&ec=GAlAmgQ&hl=ko&flowName=GlifWebSignIn&flowEntry=AddSession&dsh=S-1089772944%3A1692081081985953"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(address))
+            startActivity(intent)
+
+            val btnNaver = findViewById<ConstraintLayout>(R.id.btnNaver)
+            btnNaver.setOnClickListener {
+                val address = "https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/"
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(address))
+                startActivity(intent)
+            }
         }
     }
 }
